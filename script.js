57db49345fa6f20115c6717e2f6d5a851a28b39f
Opeten <83654834+Opeten@users.noreply.github.com>
2023-04-11 13:09:05 +0100

    Added Sudoku Solver Card

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const cards = [{ name: "Exploder", category: "Game", description: "A game where 
 { name: "Ants", category: "Simulation", description: "Ants moving around and leaving a colorful trail wherever they go.", link: "./ants/", image: "./img/ants.jpg" },
 { name: "Rubkis", category: "Game / Simulation", description: "A traditional Rubik's Cube for you to scramble and 'magically' rewind.", link: "./rubiks/", image: "./img/rubiks.jpg" },
 { name: "12 Diamonds", category: "Game", description: "12 Diamonds, one is false. Answer 3 questions and I will guess the false one!", link: "./diamonds/", image: "./img/diamonds.jpg" },
-{ name: "Mandelbrot Set", category: "Maths / Graphics", description: "Zoom in to discover intricate and beatiful patterns in this famous set.", link: "./fractals/", image: "./img/mandel.jpg" }];
+{ name: "Mandelbrot Set", category: "Maths / Graphics", description: "Zoom in to discover intricate and beatiful patterns in this famous set.", link: "./fractals/", image: "./img/mandel.jpg" },
+{ name: "Sudoku Solver (WIP)", category: "Game / Simulation", description: "Fill in the grid with what you have and it will do the rest.", link: "./SudokuSolver/", image: "./img/sudoku.png"}];
 
 var cardsContainer;
 var windowWidth;
